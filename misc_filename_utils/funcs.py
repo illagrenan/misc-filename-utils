e64c7d7132a5f72f910368a281707972de6fc3f8
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-03-24 20:47:19 +0100

    Modernize future imports.

--- misc_filename_utils/funcs.py
+++ misc_filename_utils/funcs.py
@@ -1,16 +1,12 @@
 # -*- encoding: utf-8 -*-
 # ! python2
 
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import absolute_import
-from __future__ import division
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import inspect
 import os
 
 from slugify import Slugify
-
 
 slugify_filename = Slugify()
 slugify_filename.separator = '_'
--- misc_filename_utils/tests/test_base.py
+++ misc_filename_utils/tests/test_base.py
@@ -1,25 +1,16 @@
-#!/usr/bin/python
-# coding=utf-8
+# -*- encoding: utf-8 -*-
+# ! python2
 
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import os
+from unittest import TestCase
 
 from future import standard_library
-import six
 
 from misc_filename_utils.funcs import get_safe_path_name, get_filename_from_url, upload_path
 
-
 standard_library.install_aliases()
-
-if six.PY2:
-    from unittest2 import TestCase
-else:
-    from unittest import TestCase
 
 
 class BaseTestCase(TestCase):
@@ -54,4 +45,3 @@ class BaseTestCase(TestCase):
 
         for obj, filename, expected in test_data:
             self.assertEqual(upload_path(obj, filename), expected)
-
